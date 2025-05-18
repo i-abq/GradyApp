a44586068cb629c4f51c457a0114a109fd5c8c7d
i-abq <12766451+i-abq@users.noreplay.github.com>
2025-05-18 20:20:12 -0300

    Corrige formato do middleware de monitoramento de domínio depreciado

--- config/initializers/deprecated_domain_logger.rb
+++ config/initializers/deprecated_domain_logger.rb
@@ -2,15 +2,19 @@
 # Este middleware deve ser removido após um período de transição
 # quando tivermos certeza de que não há mais requisições para esse subdomínio
 
-Rails.application.config.middleware.use(
-  lambda do |app|
-    lambda do |env|
-      request = ActionDispatch::Request.new(env)
-      if request.host == 'auth.grady.com.br'
-        Rails.logger.warn "Acesso ao domínio descontinuado: auth.grady.com.br - #{request.path}"
-        # Opcionalmente, enviar para sistemas de monitoramento (NewRelic, Datadog, etc.)
-      end
-      app.call(env)
-    end
+class DeprecatedDomainLogger
+  def initialize(app)
+    @app = app
   end
-) if Rails.env.production? 
+
+  def call(env)
+    request = ActionDispatch::Request.new(env)
+    if request.host == 'auth.grady.com.br'
+      Rails.logger.warn "Acesso ao domínio descontinuado: auth.grady.com.br - #{request.path}"
+      # Opcionalmente, enviar para sistemas de monitoramento (NewRelic, Datadog, etc.)
+    end
+    @app.call(env)
+  end
+end
+
+Rails.application.config.middleware.use DeprecatedDomainLogger if Rails.env.production? 
