04f3115f028308c97293c8ffb2709b0eb895ed9b
i-abq <12766451+i-abq@users.noreplay.github.com>
2025-05-18 19:43:25 -0300

    Fix: Renomeando DeviseController para CustomDeviseController para evitar conflito com o Devise

---
+++ app/controllers/custom_devise_controller.rb
@@ -0,0 +1,27 @@
+class CustomDeviseController < ActionController::Base
+  include Devise::Controllers::Helpers
+  
+  layout 'application'
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token, if: :json_request?
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+  
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || "https://app.grady.com.br"
+  end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    "https://www.grady.com.br"
+  end
+  
+  def json_request?
+    request.format.json?
+  end
+end 
