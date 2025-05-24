01b923a3e11a8a4e2ce3926d192c1c34c5199cd7
i-abq <12766451+i-abq@users.noreplay.github.com>
2025-05-24 20:37:54 -0300

    Fix logout redirect issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :check_auth_subdomain
   
+  protected
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+  
   private
   
   def check_auth_subdomain
