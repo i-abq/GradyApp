35a3d7f31795a236446af7fad78210c9fb2634d2
i-abq <12766451+i-abq@users.noreplay.github.com>
2025-06-30 01:41:21 -0300

    refactor: Use Stimulus targets for sidebar controller

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -3,24 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["sidebar", "overlay"]
 
-  connect() {
-    this.sidebar = document.getElementById('sidebar');
-    this.overlay = document.getElementById('page-overlay');
-    this.menuButton = document.getElementById('menu-toggle-btn');
-    this.closeButton = document.getElementById('sidebar-close-btn');
-
-    this.menuButton.addEventListener('click', () => this.open());
-    this.closeButton.addEventListener('click', () => this.close());
-    this.overlay.addEventListener('click', () => this.close());
-  }
-
   open() {
-    this.sidebar.classList.add('is-open');
-    this.overlay.classList.add('is-active');
+    this.sidebarTarget.classList.add('is-open');
+    this.overlayTarget.classList.add('is-active');
   }
 
   close() {
-    this.sidebar.classList.remove('is-open');
-    this.overlay.classList.remove('is-active');
+    this.sidebarTarget.classList.remove('is-open');
+    this.overlayTarget.classList.remove('is-active');
   }
 } 
